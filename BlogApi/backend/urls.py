e932206a8f88899c47541447908a4cbb08072b15
banset <mohitbasnet80@gmail.com>
2024-02-29 15:44:43 +0545

    Added the put and delete api

--- BlogApi/backend/urls.py
+++ BlogApi/backend/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import article_list
+from . views import article_list,article_detials
 
 urlpatterns = [
-    path('articles/', article_list)
+    path('articles/', article_list),
+    path('articles/<slug:slug>/', article_detials)
 ]
--- BlogApi/backend/views.py
+++ BlogApi/backend/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 from . models import Article
@@ -21,3 +21,39 @@ def article_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status = 201)
         return JsonResponse(serializer.errors, status = 400)
+
+
+
+
+
+
+@csrf_exempt
+def article_detials(request,slug):
+
+    try:
+        article = Article.objects.get(slug=slug)
+    except Article.DoesNotExist:
+        return HttpResponse(status = 404)
+    
+
+    if request.method == "GET":
+        serializer = ArticleSerializer(article)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = ArticleSerializer(article, data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 200)
+        return JsonResponse(serializer.errors, staturs = 400)
+
+    
+    elif request.method == "DELETE":
+        article.delete()
+        return HttpResponse(status=404)
+
+
+
+
+    
