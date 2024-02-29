4ce69fa6273331d2f56649853b0f8561237f9e68
banset <mohitbasnet80@gmail.com>
2024-02-29 14:45:53 +0545

    Added view funtion for the API

--- BlogApi/backend/serializers.py
+++ BlogApi/backend/serializers.py
@@ -2,9 +2,11 @@ from rest_framework import serializers
 from . models import Article
 
 class ArticleSerializer(serializers.ModelSerializer):
+    slug = serializers.SlugField(read_only=True)
     class Meta:
         model = Article
         fields = '__all__'
+
 # class ArticleSerializer(serializers.Serializer):
 #     title = serializers.CharField(max_length=200)
 #     description = serializers.CharField()
