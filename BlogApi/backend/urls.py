83c11945ec69f4194cf622760ed9287bd7c5b8fe
banset <mohitbasnet80@gmail.com>
2024-03-04 15:44:29 +0545

    Added ModelViewSet and default Router

--- BlogApi/backend/urls.py
+++ BlogApi/backend/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from . views import ArticleList,ArticleDetails
+from django.urls import path,include
+# from . views import ArticleList,ArticleDetails
+
+
+from rest_framework.routers import DefaultRouter
+from . views import ArticleViewSet
+router = DefaultRouter()
+router.register('articles', ArticleViewSet ,basename = 'articles')
+
 urlpatterns = [
-    path('articles/', ArticleList.as_view()),
-    path('articles/<slug:slug>/',ArticleDetails.as_view() ),
+    # path('articles/', ArticleList.as_view()),
+    # path('articles/<slug:slug>/',ArticleDetails.as_view() ),
+      path('',include(router.urls)) 
 ]
