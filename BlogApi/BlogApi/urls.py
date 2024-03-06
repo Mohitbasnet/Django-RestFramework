969f16fc7809cb3e5d3e1e0dff00c06d0df4ef66
banset <mohitbasnet80@gmail.com>
2024-03-06 13:53:40 +0545

    Added django rest auth

--- BlogApi/BlogApi/urls.py
+++ BlogApi/BlogApi/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import re_path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('backend.urls')),
-   path('api-auth/', include('rest_framework.urls')), # this is for browsable api login/logout for this use django==3.1.14
-
+    re_path('admin/', admin.site.urls),
+    re_path('',include('backend.urls')),
+    re_path('api-auth/', include('rest_framework.urls')), # this is for browsable api login/logout for this use django==3.1.14
+    re_path('rest-auth/', include('rest_auth.urls')),
+    re_path('rest-auth/registration/', include('rest_auth.registration.urls'))
 ]
